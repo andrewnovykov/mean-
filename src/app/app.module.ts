b1d5446fbc9c973ddb33eed56e0a60e0c5d66fe9
papapin <papapin777#gmail.com>
2018-06-19 22:08:49 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './post/post-create/post-create.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule
